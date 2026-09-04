13d7368a395635571b8c4ccb0887eb68acb8d06f
DJ Mahir Nation TV <78599090+DJMahirNationTV@users.noreply.github.com>
2026-09-04 18:41:32 +0200

    fix: naming Issues

--- backend/src/main/java/org/movielib/backend/controller/movieController.java
+++ backend/src/main/java/org/movielib/backend/controller/movieController.java
@@ -1,22 +1,22 @@
 package org.movielib.backend.controller;
 
-import org.movielib.backend.dto.OmdbMovieResponse;
-import org.movielib.backend.service.MovieService;
+import org.movielib.backend.dto.omdbMovieResponse;
+import org.movielib.backend.service.movieService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/movies")
 @CrossOrigin(origins = "http://localhost:5173") // for now, I would say, its 5173, but we can change it later...
-public class MovieController {
-    private final MovieService movieService;
+public class movieController {
+    private final movieService movieService;
 
-    public MovieController(MovieService movieService) {
+    public movieController(movieService movieService) {
         this.movieService = movieService;
     }
     @GetMapping("/{imdbId}")
-    public ResponseEntity<OmdbMovieResponse> getMovie(@PathVariable String imdbId) {
-        OmdbMovieResponse movie = movieService.getMovieByImdbId(imdbId);
+    public ResponseEntity<omdbMovieResponse> getMovie(@PathVariable String imdbId) {
+        omdbMovieResponse movie = movieService.getMovieByImdbId(imdbId);
         return ResponseEntity.ok(movie);
     }
 }
--- backend/src/main/java/org/movielib/backend/dto/omdbMovieResponse.java
+++ backend/src/main/java/org/movielib/backend/dto/omdbMovieResponse.java
@@ -2,7 +2,7 @@ package org.movielib.backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 // We have to check, which fields we want here... For now, I initzalized the fields that I thought of..
-public record OmdbMovieResponse(
+public record omdbMovieResponse(
         @JsonProperty("Title") String title,
         @JsonProperty("Year") String year,
         @JsonProperty("imdbID") String imdbId,

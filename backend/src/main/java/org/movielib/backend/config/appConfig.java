132e1d17d8f4c362ba1f6a8217a63d42ba7b9ca6
DJ Mahir Nation TV <78599090+DJMahirNationTV@users.noreply.github.com>
2026-09-04 18:42:41 +0200

    fix: naming Issues

--- backend/src/main/java/org/movielib/backend/config/appConfig.java
+++ backend/src/main/java/org/movielib/backend/config/appConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-public class AppConfig {
+public class appConfig {
 
     @Value("${omdb.api.base-url}") // Fetch the API URL from application.properties
     private String omdbBaseUrl;

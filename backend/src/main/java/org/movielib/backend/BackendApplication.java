aa0963ad1d6f56828522db29f0a7ab353c44bf0a
DJ Mahir Nation TV <78599090+DJMahirNationTV@users.noreply.github.com>
2026-09-08 16:20:35 +0200

    added comment in German instead of English, for better understanding :) (#20)
    * fix: fixing issues from Basak
    
    * fix: fixing issues from Basak
    
    * fix: Mongo Database was missing
    
    * fix: fixing stuff
    
    * fix: fixing stuff
    
    * fix: errors on something, that caused too heavy bugs
    
    * fix: restore proper movie controller
    
    * fix: restore proper movie service
    
    * fix: finally fixed the backend Issues, the API is now requesting stuff properly
    
    * feat: added workflow to deploy all the stuff
    
    * fix: actions/setup-buildx-action@v3 to docker/setup-buildx-action@v3
    
    * fix: added comment in German instead of English, for better understanding :)
    
    fix: renamed 21 to 25 (name of a Workflow)

--- backend/src/main/java/org/movielib/backend/BackendApplication.java
+++ backend/src/main/java/org/movielib/backend/BackendApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args); // Hier wird einfach die Spring Boot Anwendung gestartet. bzw, passiert nichts :)
     }
 }

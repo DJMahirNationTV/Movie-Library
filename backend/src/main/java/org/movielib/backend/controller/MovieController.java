9cde5b884acdb7a9430567738218791a825ac46b
DJ Mahir Nation TV <78599090+DJMahirNationTV@users.noreply.github.com>
2026-09-08 12:43:56 +0200

    Ticket 006 (#15)
    * fix: fixing issues from Basak
    
    * fix: fixing issues from Basak
    
    * fix: Mongo Database was missing
    
    * fix: fixing stuff
    
    * fix: fixing stuff
    
    * fix: errors on something, that caused too heavy bugs
    
    * fix: restore proper movie controller
    
    * fix: restore proper movie service

--- backend/src/main/java/org/movielib/backend/controller/MovieController.java
+++ backend/src/main/java/org/movielib/backend/controller/MovieController.java
@@ -1,16 +1,24 @@
 package org.movielib.backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.movielib.backend.dto.omdbMovieResponse;
 import org.movielib.backend.model.Movie;
 import org.movielib.backend.service.MovieService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/movies")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class MovieController {
 
     private final MovieService movieService;
@@ -23,5 +31,10 @@ public class MovieController {
     @PostMapping
     public Movie addMovie(@RequestBody Movie movie) {
         return movieService.addMovie(movie);
+    }
+
+    @GetMapping("/omdb/{imdbId}")
+    public ResponseEntity<omdbMovieResponse> getOmdbMovie(@PathVariable String imdbId) {
+        return ResponseEntity.ok(movieService.fetchMovieFromOmdb(imdbId));
     }
 }

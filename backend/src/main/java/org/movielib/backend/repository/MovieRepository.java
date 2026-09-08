b359e0d23253e6a075425862055b4da48c9fcd5b
basak-tahtaci <basaktahtaci@gmail.com>
2026-09-08 10:08:40 +0200

    updated application.properties (#8)
    Co-authored-by: basak-tahtaci <basaktahtaci@MacBook-Air-von-Basak.local>

---
+++ backend/src/main/java/org/movielib/backend/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package org.movielib.backend.repository;
+
+import org.movielib.backend.model.Movie;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, String> {
+}

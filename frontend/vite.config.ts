4cb736a9dc0390caa93426adc63a2d00d5b40980
DJ Mahir Nation TV <78599090+DJMahirNationTV@users.noreply.github.com>
2026-09-11 16:09:12 +0200

    feat: added backend URL for Production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import env from "dotenv";
+env.config();
+
+const backendUrl = process.env.BACKEND_URL || "http://localhost:8080"; // || bedeuetet "Fallback"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  }
-})
+      "/api": backendUrl,
+    },
+  },
+});
